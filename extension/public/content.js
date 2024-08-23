c6e3ce5e8ae647218aa3a02d67259a41d2c23acc
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-08-22 22:14:54 -0300

    refactor: add indexed db control

--- extension/public/content.js
+++ extension/public/content.js
@@ -6,16 +6,30 @@
   window.autoDetectEnabled = autoDetectEnabled;
 
   console.log("Injected script running");
-  setInterval(() => {
-    if (window.autoDetectEnabled) {
+  setInterval(async () => {
+    let autoDetectEnabled =
+      localStorage.getItem("autoDetectEnabled") === "true";
+    window.autoDetectEnabled = autoDetectEnabled;
+    if (!autoDetectEnabled) return;
+
+    const name = getChatName();
+    const warningIgnored = await checkWarningStatus(name);
+    if (warningIgnored == true || warningIgnored === undefined) {
       scrapData();
     }
-  }, 10000);
+  }, 5000);
 })();
 
-var reportedMessages = new Set();
+function getChatName() {
+  const currentMain = document.querySelector("#main header");
+  if (!currentMain) return null;
+  const name = currentMain.getElementsByTagName("span")[0]?.textContent || null;
+  return name === "default-user"
+    ? currentMain.getElementsByTagName("span")[1]?.textContent || null
+    : name;
+}
 
-function scrapData() {
+async function scrapData() {
   console.log("scrapData called");
   let main = document.getElementById("main");
   if (!main) {
@@ -23,13 +37,7 @@ function scrapData() {
     return;
   }
 
-  let header = main?.getElementsByTagName("header")[0];
-  let photo = header?.getElementsByTagName("img")[0]?.src || "";
-
-  let nameIndex = photo ? 0 : 1;
-  let name = header?.getElementsByTagName("span")[nameIndex]?.textContent || "";
   let texts = main?.querySelectorAll("[data-pre-plain-text]");
-
   let messages = [];
   if (texts) {
     for (let i = 0; i < texts.length; i++) {
@@ -49,17 +57,34 @@ function scrapData() {
     }
   }
 
+  if (messages.length > 10) messages = messages.slice(-10);
+
   let messagesString = JSON.stringify(messages);
   let messagesHash = generateHash(messagesString);
 
-  if (!reportedMessages.has(messagesHash)) {
-    reportedMessages.add(messagesHash);
+  // if (!reportedMessages.has(messagesHash)) {
+  if (!(await checkIfMessageExists(messagesHash))) {
+    let header = main?.getElementsByTagName("header")[0];
+    let photo = header?.getElementsByTagName("img")[0]?.src || "";
+    let nameIndex = photo ? 0 : 1;
+    let name =
+      header?.getElementsByTagName("span")[nameIndex]?.textContent || "";
+
+    // reportedMessages.add(messagesHash);
+    await storeMessage(messagesHash, { name, photo, messages });
     reportUser({ name, photo, messages });
-    console.log("New messages reported", { name, photo, messages });
+    console.log("New messages reported", {
+      name,
+      photo,
+      messages,
+      messagesHash,
+    });
+
+    return true;
   } else {
     console.log("Messages already reported");
+    return false;
   }
-  return { name, photo, messages };
 }
 
 function generateHash(str) {
@@ -86,17 +111,22 @@ async function reportUser(data) {
       }),
     });
     const json = await res.json();
-    if (json.note > 6) {
+
+    console.log("Server response:", json);
+    if (json.note >= 8) {
       // alert(`User ${data?.name} reported. Chance of fraud: ${json.note}`);
-      addWarningOverlay();
+      addWarningOverlay(data.name);
     }
   } catch (err) {
     console.error("Error fetching data:", err);
   }
 }
 
-function addWarningOverlay() {
+async function addWarningOverlay(name) {
+  if (document.getElementById("overlay-zap-defender")) return;
+
   var overlay = document.createElement("div");
+  overlay.id = "overlay-zap-defender";
   overlay.style.position = "fixed";
   overlay.style.top = 0;
   overlay.style.left = 0;
@@ -104,10 +134,11 @@ function addWarningOverlay() {
   overlay.style.height = "100%";
   overlay.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
   overlay.style.zIndex = 1000;
-  overlay.style.cursor = "pointer";
   overlay.style.display = "flex";
+  overlay.style.flexDirection = "column";
   overlay.style.justifyContent = "center";
   overlay.style.alignItems = "center";
+  overlay.style.gap = "8px";
 
   // Cria o aviso no centro do overlay
   var warningText = document.createElement("div");
@@ -117,13 +148,153 @@ function addWarningOverlay() {
   warningText.style.fontWeight = "bold";
   warningText.style.textAlign = "center";
 
+  // Cria o aviso no centro do overlay
+  var ignoreButton = document.createElement("div");
+  ignoreButton.textContent = "ignorar";
+  ignoreButton.style.color = "white";
+  ignoreButton.style.fontSize = "16px";
+  ignoreButton.style.textAlign = "center";
+  ignoreButton.style.cursor = "pointer";
+
+  // Adiciona um evento de clique para remover o overlay
+  ignoreButton.addEventListener("click", async function () {
+    overlay.remove();
+    await storeWarningStatus(name, true);
+  });
+
   // Adiciona o aviso ao overlay
   overlay.appendChild(warningText);
-  // Adiciona um evento de clique para remover o overlay
-  overlay.addEventListener("click", function () {
-    overlay.remove();
-  });
+  overlay.appendChild(ignoreButton);
 
   // Adiciona o overlay na div #main
   document.querySelector("#main").appendChild(overlay);
+  await storeWarningStatus(name, false);
+}
+
+// Função para monitorar mudanças no #main
+var lastOpenChat = null;
+
+function monitorMainChanges() {
+  setInterval(async () => {
+    let autoDetectEnabled =
+      localStorage.getItem("autoDetectEnabled") === "true";
+    window.autoDetectEnabled = autoDetectEnabled;
+    if (!autoDetectEnabled) return;
+
+    let name = getChatName();
+
+    if (name && lastOpenChat !== name) {
+      console.log("Last:", lastOpenChat);
+      lastOpenChat = name;
+      console.log("Checking", name);
+      const warningIgnored = await checkWarningStatus(name);
+
+      if (warningIgnored == false) {
+        addWarningOverlay(name);
+      }
+    }
+  }, 1000);
+}
+
+// Inicia o monitoramento
+monitorMainChanges();
+
+function openDatabase() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open("MessageDatabase", 1);
+
+    request.onupgradeneeded = (event) => {
+      const db = event.target.result;
+      if (!db.objectStoreNames.contains("messages")) {
+        db.createObjectStore("messages", { keyPath: "hash" });
+      }
+      if (!db.objectStoreNames.contains("warnings")) {
+        const store = db.createObjectStore("warnings", { keyPath: "name" });
+        store.createIndex("name", "name", { unique: true });
+      }
+    };
+
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    };
+
+    request.onerror = (event) => {
+      reject("Erro ao abrir IndexedDB:", event.target.errorCode);
+    };
+  });
+}
+
+async function storeMessage(hash, data) {
+  const db = await openDatabase();
+  const transaction = db.transaction(["messages"], "readwrite");
+  const objectStore = transaction.objectStore("messages");
+
+  return new Promise((resolve, reject) => {
+    const request = objectStore.add({ hash: hash, data: data });
+    request.onsuccess = () => {
+      resolve(true);
+    };
+    request.onerror = () => {
+      resolve(false);
+    };
+  });
+}
+
+async function checkIfMessageExists(hash) {
+  const db = await openDatabase();
+  const transaction = db.transaction(["messages"], "readonly");
+  const objectStore = transaction.objectStore("messages");
+
+  return new Promise((resolve) => {
+    const request = objectStore.get(hash);
+    request.onsuccess = () => {
+      resolve(!!request.result);
+    };
+    request.onerror = () => {
+      resolve(false);
+    };
+  });
+}
+
+async function checkWarningStatus(name) {
+  const db = await openDatabase();
+  const transaction = db.transaction(["warnings"], "readonly");
+  const store = transaction.objectStore("warnings");
+
+  console.log("Checking latest warning for", name);
+
+  return new Promise((resolve) => {
+    const index = store.index("name");
+    const cursorRequest = index.openCursor(name, "prev"); // "prev" para iterar na ordem inversa
+
+    cursorRequest.onsuccess = (event) => {
+      const cursor = event.target.result;
+      if (cursor) {
+        console.log(`Last warning for ${name}:`, cursor.value);
+        resolve(cursor.value.ignored);
+      } else {
+        resolve(undefined); // Não encontrou nenhum registro
+      }
+    };
+
+    cursorRequest.onerror = () => {
+      resolve(undefined); // Em caso de erro na operação
+    };
+  });
+}
+
+async function storeWarningStatus(name, ignored) {
+  const db = await openDatabase();
+  const transaction = db.transaction(["warnings"], "readwrite");
+  const store = transaction.objectStore("warnings");
+
+  return new Promise((resolve) => {
+    const request = store.put({ name: name, ignored: ignored });
+    request.onsuccess = () => {
+      resolve(true);
+    };
+    request.onerror = () => {
+      resolve(false);
+    };
+  });
 }
