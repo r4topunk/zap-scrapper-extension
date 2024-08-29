46d184a451708f4257786accb4934b0c425a90e1
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-08-29 02:32:28 -0300

    refactor: update whatsapp filename

--- extension/public/background.js
+++ extension/public/background.js
@@ -1,9 +1,12 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === 'complete' && /^https:\/\/web.whatsapp.com/.test(tab.url)) {
+    if (
+      changeInfo.status === "complete" &&
+      /^https:\/\/web.whatsapp.com/.test(tab.url)
+    ) {
       chrome.scripting.executeScript({
         target: { tabId: tabId },
-        files: ['content.js']
+        files: ["whatsapp.js"],
       });
     }
   });
