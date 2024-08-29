2d6bc48ecefd5502410efc8637241c9caf46332f
Gustavo Kuhl <gustavo-kuhl@hotmail.com>
2024-08-29 02:54:11 -0300

    feat: add cnx from page to extension

--- extension/public/background.js
+++ extension/public/background.js
@@ -11,3 +11,29 @@ chrome.runtime.onInstalled.addListener(() => {
     }
   });
 });
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (
+      changeInfo.status === "complete" &&
+      /^https:\/\/ruxintel.r4topunk.xyz/.test(tab.url)
+    ) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["ruxintel.js"],
+      });
+    }
+  });
+});
+
+// Escuta mensagens do content script
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.jwtToken) {
+    // Armazena o JWT em chrome.storage.local
+    chrome.storage.local.set({ jwtToken: request.jwtToken }, function () {
+      console.log("JWT salvo em chrome.storage.local.");
+      sendResponse({ status: "JWT armazenado com sucesso" });
+    });
+  }
+  return true; // Indica que a resposta será enviada de forma assíncrona
+});
--- extension/public/ruxintel.js
+++ extension/public/ruxintel.js
@@ -1,1 +1,27 @@
-console.log("teste");
+console.log("Ruxintel content loaded");
+
+const checkInterval = 5000; // 5 segundos
+
+function checkLocalStorage() {
+  const jwt = localStorage.getItem("jwt");
+
+  if (jwt) {
+    chrome.runtime.sendMessage({ jwtToken: jwt }, function (response) {
+      if (chrome.runtime.lastError) {
+        console.error(
+          "Erro ao enviar mensagem para a extensão:",
+          chrome.runtime.lastError
+        );
+      } else {
+        console.log(
+          "JWT enviado para a extensão. Resposta:",
+          response ? response.status : "sem resposta"
+        );
+      }
+    });
+  } else {
+    console.log("Nenhum JWT encontrado no localStorage.");
+  }
+}
+
+setInterval(checkLocalStorage, checkInterval);
